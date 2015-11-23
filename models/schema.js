fcecebfa98ca981a693f3501184ebb366f49c79e
Rishad <rrrahman@mit.edu>
2015-11-22 21:01:09 -0500

    Cleaned up error handling

--- models/schema.js
+++ models/schema.js
@@ -16,43 +16,11 @@ var UserSchema = mongoose.Schema({
   requestsTaken: [{type: Schema.Types.ObjectId, ref:'Request'}]
 });
 
-UserSchema.statics.verifyPassword = function(user, candidatepw, cb){
-  var that = this;
-  that.find({username: user}, function(err, userQuery){
-    if (err) cb(err);
-    else if (userQuery.length == 0) cb({msg: "No such user!"});
-    else if (userQuery.length > 1) cb({msg: "Multiple usernames exist!"});
-    else {
-      if (candidatepw === userQuery[0].password) cb(null, true);
-      else cb(null, false);
-    }
-  });
-}
-
-UserSchema.statics.createNewUser = function(user, password, cb){
-  var that = this;
-  that.find({username: user}, function(err, userQuery){
-    if (err) cb(err);
-    else if (userQuery.length == 0) {
-      that.create({
-        'username': user,
-        'password': password,
-        'myRequests': [],
-        'requestsTaken': []
-      },  function(err, res) {
-        if (err) cb(err);
-        else cb(null, false); //not taken
-      });
-    }
-    else cb(null, true); //taken
-  });
-}
-
 UserSchema.statics.getUser = function(user, cb){
   var that = this;
   that.find({ username: user }, function(err, userQuery){
     if (err) {
-      cb(err);
+      cb({err: "Failed to query user"});
     }
     else if (userQuery.length == 0) {
       cb({msg: "No such user!"});
@@ -66,68 +34,101 @@ UserSchema.statics.getUser = function(user, cb){
   });
 }
 
+UserSchema.statics.verifyPassword = function(user, candidatepw, cb){
+  var that = this;
+  that.getUser(user, function(err, user){
+    if (err) cb(err);
+    else if (candidatepw === user.password) cb(null, true);
+    else cb(null, false);
+  });
+}
+
+UserSchema.statics.createNewUser = function(user, password, cb){
+  var that = this;
+  that.getUser(user, function(err, user){
+    if (err) {
+      if (err.msg && err.msg === "No such user!"){
+        cb(null, true);//taken;
+      }
+      else cb(err);
+    }
+    else {
+      that.create({
+        'username': user.username,
+        'password': password,
+        'myRequests': [],
+        'requestsTaken': []
+      },  function(err, res) {
+        if (err) cb({err: "Failed to create user"});
+        else cb(null, false); //not taken
+      });
+    }
+  });
+}
+
 UserSchema.statics.getUserData = function(user, cb){
   var that = this;
   that.getUser(user, function(err, user){
-   if (err) cb(err);
+   if (err) cb({err: "Failed to query user"});
    that.populate(user, {path: 'myRequests'}, function(err, result){
-    if (err) cb(err);
+    if (err) cb({err: "Failed to populate user requests"});
     else {
       that.populate(result, {path: 'requestsTaken'}, function(err, result){
-        if (err) cb(err);
-          else cb(null, result); //candidates and helpers not populated
-        });
+        if (err) cb({err: "Failed to populate requests"});
+        else {
+          that.populate(result, {path: 'myRequests.candidates', model: 'User'}, function(err, result){
+            if (err) cb({err: "Failed to populate candidates"});
+            else {
+              that.populate(result, {path: 'myRquests.helpers', model'User'}, function(err, result){
+                if (err) cb({err: "Failed to populate helpers"});
+                else cb(null, result);
+              });
+            }
+          });
+        } 
+      });
     } 
   });
  });
 }
 
-UserSchema.statics.getUserRequests = function(user, cb) {
-  var that = this;
-  that.find({ username: user }, function(err, userQuery){
-    if (err) cb(err);
-    else if (userQuery.length == 0) cb({msg: "No such user!"});
-    else if (userQuery.length > 1) cb({msg: "Multiple usernames exist!"});
-    else {
-      that.populate(userQuery[0], {path: 'myRequests'}, function(err, result){
-        if (err) cb(err);
-        else cb(null, result); //candidates and helpers not populated
-      });
-    }
-  });
-}
+// UserSchema.statics.getUserRequests = function(user, cb) {
+//   var that = this;
+//   that.find({ username: user }, function(err, userQuery){
+//     if (err) cb(err);
+//     else if (userQuery.length == 0) cb({msg: "No such user!"});
+//     else if (userQuery.length > 1) cb({msg: "Multiple usernames exist!"});
+//     else {
+//       that.populate(userQuery[0], {path: 'myRequests'}, function(err, result){
+//         if (err) cb(err);
+//         else cb(null, result); //candidates and helpers not populated
+//       });
+//     }
+//   });
+// }
 
-UserSchema.statics.getRequestsTaken = function(user, cb){
-  var that = this;
-  that.find({ username: user }, function(err, userQuery){
-    if (err) cb(err);
-    else if (userQuery.length == 0) cb({msg: "No such user!"});
-    else if (userQuery.length > 1) cb({msg: "Multiple usernames exist!"});
-    else {
-      that.populate(userQuery[0], {path: 'requestsTaken'}, function(err, result){
-        if (err) cb(err);
-        else cb(null, result); //candidates and helpers not populated
-      });
-    }
-  });
-}
+// UserSchema.statics.getRequestsTaken = function(user, cb){
+//   var that = this;
+//   that.find({ username: user }, function(err, userQuery){
+//     if (err) cb(err);
+//     else if (userQuery.length == 0) cb({msg: "No such user!"});
+//     else if (userQuery.length > 1) cb({msg: "Multiple usernames exist!"});
+//     else {
+//       that.populate(userQuery[0], {path: 'requestsTaken'}, function(err, result){
+//         if (err) cb(err);
+//         else cb(null, result); //candidates and helpers not populated
+//       });
+//     }
+//   });
+// }
 
 UserSchema.statics.addRequest = function(user, requestId, cb){
   var that = this;
-  that.find({ username: user }, function(err, userQuery){
-    console.log("raw error: ", err); //debug
+  that.getUser(user, function(err, user){
     if (err) cb(err);
-    else if (userQuery.length == 0) {
-      console.error("no such user"); //debug
-      cb({msg: "No such user!"});
-    }
-    else if (userQuery.length > 1) {
-      console.error("multiple usernames exist!"); //debug
-      cb({msg: "Multiple usernames exist!"});
-    }
     else {
       that.update(userQuery[0],{$push: {'myRequests': requestId}},{upsert:true},function(err){
-        if (err) cb(err);
+        if (err) cb({err: "Failed to add user request"});
         else cb(null);
       });
     }
@@ -136,13 +137,11 @@ UserSchema.statics.addRequest = function(user, requestId, cb){
 
 UserSchema.statics.removeRequest = function(user, requestModel, requestId, cb){
   var that = this;
-  that.find({ username: user }, function(err, userQuery){
+  that.getUser(user, function(err, user){
     if (err) cb(err);
-    else if (userQuery.length == 0) cb({msg: "No such user!"});
-    else if (userQuery.length > 1) cb({msg: "Multiple usernames exist!"});
     else {
       that.update(userQuery[0],{$pull: {'myRequests': requestId}},{upsert:true},function(err){
-        if(err) cb(err);
+        if(err) cb({err: "Failed to remove user request"});
         else {
           requestModel.removeRequest(requestId, function(err){
             if (err) cb(err);
@@ -175,21 +174,17 @@ var RequestSchema = mongoose.Schema({
 RequestSchema.statics.getRequestById = function(requestId, cb){
   var that = this;
   that.find({ _id: requestId }, function(err, requestQuery){
-    if (err) cb(err);
-    else if (requestQuery.length == 0)
-    {
-      console.log("requestQuery length is 0");
-    }
+    if (err) cb({err: "Failed to query request"});
     else {
       that.populate(requestQuery, {path: 'creator'}, function(err, requestQuery){ //creator populated
-        if (err) cb(err);
+        if (err) cb({err: "Failed to populate creators"});
         else {
           that.populate(requestQuery, {path: 'candidates'}, function(err, result){
-            if (err) cb(err);
+            if (err) cb({err: "Failed to populate candidates"});
             else {
               that.populate(result[0], {path: 'helpers'}, function(err, result){
-                if(err) cb(err);
-                else cb(null, result[0]); //candidates and helpers populated
+                if(err) cb({err: "Failed to populate helpers"});
+                else cb(null, result[0]);
               });
             }
           });
@@ -202,17 +197,17 @@ RequestSchema.statics.getRequestById = function(requestId, cb){
 RequestSchema.statics.getAllRequests = function(cb){
   var that = this;
   that.find({}, function(err, requestQuery){
-    if (err) cb(err);
+    if (err) cb({err: "Failed to query request"});
     else {
       that.populate(requestQuery, {path: 'creator'}, function(err, requestQuery){ //creator populated
-        if (err) cb(err);
+        if (err) cb({err: "Failed to populate creators"});
         else {
           that.populate(requestQuery, {path: 'candidates'}, function(err, result){
-            if (err) cb(err);
+            if (err) cb({err: "Failed to populate candidates"});
             else {
               that.populate(result, {path: 'helpers'}, function(err, result){
-                if(err) cb(err);
-                else cb(null, result); //candidates and helpers populated
+                if(err) cb({err: "Failed to populate helpers"});
+                else cb(null, result);
               });
             }
           });
@@ -228,14 +223,14 @@ RequestSchema.statics.createRequest = function(userModel, user, requestData, cb)
   requestData.candidates = [];
   requestData.helpers = [];
   userModel.getUser(user, function(err, user){
-    if (err) cb({msg: "Error getting user when creating request"});
+    if (err) cb(err);
     else {
       requestData.creator = user;
       that.create(requestData, function(err, request){
-        if (err) cb({msg: "Error creating request"});
+        if (err) cb({err: "Failed to create request"});
         else {
           userModel.addRequest(user.username, request, function(err){
-            if (err) cb({msg: "Error adding request"});
+            if (err) cb(err);
             else cb(null);
           });
         }
@@ -247,7 +242,7 @@ RequestSchema.statics.createRequest = function(userModel, user, requestData, cb)
 RequestSchema.statics.removeRequest = function(requestId, cb){
   var that = this;
   that.remove({ _id: requestId }, function(err){
-    if (err) cb(err);
+    if (err) cb({err: "Failed to remove request"});
     else cb(null);
   });
 }
@@ -258,10 +253,13 @@ RequestSchema.statics.addCandidate = function(requestId, userModel, candidate, c
     if (err) cb(err);
     else {
       that.getRequestById(requestId, function(err, result){
-        that.update(result, {$push: {'candidates': candidate}}, {upsert: true}, function(err){
-          if (err) cb(err);
-          else cb(null);
-        });
+        if (err) cb(err);
+        else {
+          that.update(result, {$push: {'candidates': candidate}}, {upsert: true}, function(err){
+            if (err) cb({err: "Failed to add candidate"});
+            else cb(null);
+          });
+        }
       });
     }
   });
@@ -273,10 +271,13 @@ RequestSchema.statics.acceptCandidate = function(requestId, candidate, cb){
     if (err) cb(err);
     else {
       that.getRequestById(requestId, function(err, result){
-        that.update(result, {$pull: {'candidates': candidate}, $push: {'helpers': candidate}}, {upsert: true}, function(err){
-          if (err) cb(err);
-          else cb(null);
-        });
+        if (err) cb(err);
+        else {
+          that.update(result, {$pull: {'candidates': candidate}, $push: {'helpers': candidate}}, {upsert: true}, function(err){
+            if (err) cb({err: "Failed to accept candidate"});
+            else cb(null);
+          });
+        }
       });
     }
   });
