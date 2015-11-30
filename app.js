e18f7d256ccb400116e87079394e0ac00749aa77
unknown <wgwong@mit.edu>
2015-11-30 15:25:34 -0500

    ui changes

--- app.js
+++ app.js
@@ -10,14 +10,16 @@ var bodyParser = require("body-parser");
 var index = require('./routes/index');
 var users = require('./routes/users');
 var requests = require('./routes/requests');
+var profile = require('./routes/profile');
 
 //import backend database handler
 var mongoose = require("mongoose");
 
 //connect to backend database
-
 mongoose.connect(process.env.MONGOLAB_URI || 'mongodb://localhost:27017/requestr'); // connect to our database
 
+//import passport
+//var SamlStrategy = require("passport-saml").Strategy;
 
 // Import User model
 var Schema = require('./models/schema')
@@ -63,6 +65,7 @@ app.use(function(req, res, next) {
 app.use('/', index);
 app.use('/users', users);
 app.use('/requests', requests);
+app.use('/profile', profile);
 
 
 // ERROR HANDLERS
