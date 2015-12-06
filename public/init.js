ed9cde3b109005bb31ca02dfe78ebe843b89fb7f
unknown <wgwong@mit.edu>
2015-12-06 14:54:19 -0500

    fixed expire date display issue

--- public/init.js
+++ public/init.js
@@ -1,5 +1,10 @@
 $(document).ready(function() {
 	$('[data-toggle="tooltip"]').tooltip(); //initializes all bootstrap tooltips
-	$('input[name="requestExpiresTime"]').ptTimeSelect(); //initializes the expiration date tool
-	$("#ptTimeSelectCntr").css("z-index", 1051); //manually force it to show in front of modal, default z-index is not high enough
+	try { //we don't need to import pttimeselectcntr for request.ejs and profile.ejs, but they still call init.js
+		//therefore, we put this in a try statement and ignore if it fails
+		$('input[name="requestExpiresTime"]').ptTimeSelect(); //initializes the expiration date tool
+		$("#ptTimeSelectCntr").css("z-index", 1051); //manually force it to show in front of modal, default z-index is not high enough
+	} catch (err) {
+
+	}
 });
